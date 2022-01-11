00435e7e1a778615f6ce8d833ba098a896a33cf6
gozixq <natsinee2545@gmail.com>
2022-01-11 16:52:34 +0700

    ###################

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,57 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+
+
+
+void showMatrix(const bool [][N]);
+
+int main(){
+	double A[N][N]; 
+	bool B[N][N]; 
+	cout << "Input the matrix.....\n";
+	inputMatrix(A);
+	findLocalMax(A,B);
+	cout << "Result is \n";
+	showMatrix(B);
+	return 0;
+}
+
+void showMatrix(const bool A[][N])
+{
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			cout << A[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+
+
+void findLocalMax(const double P[][N], bool B[][N]){
+    for (int k = 0; k < N; k++)
+    {
+        for (int l = 0; l < N; l++)
+        {
+            if (P[k][l]>=P[k][l-1] && P[k][l]>=P[k][l+1] && P[k][l]>=P[k-1][l] && P[k][l]>=P[k+1][l])
+            {
+                B[k][l] =true;
+            }else B[k][l]=false;
+            
+        }
+        
+    }
+    for (int i = 0; i<N; i++){
+        B[0][i] =0;
+        B[N-1][i] =0;
+    }
+    for (int j = 0; j < N-2; j++){
+        B[j+1][0]=0;
+        B[j+1][N-1]=0;
+    }
+    
+}
