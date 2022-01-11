3509536a37ba428d44e391aba133363229af3ebe
gozixq <natsinee2545@gmail.com>
2022-01-11 16:51:15 +0700

    #######################

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int k = 1; k<N ; k++)
+	{
+		for(int i=k; i>0;i--)
+		{
+			if(d[i]>d[i-1]){
+				double temp = d[i];
+				d[i] =d[i-1];
+				d[i-1]=temp;
+			}
+		}
+		
+		cout << "Pass " << k << ":" ;
+		
+		for(int y=0; y<N ; y++)
+		{
+			cout << d[y] << " ";
+		}
+		cout << "\n";
+	}
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
